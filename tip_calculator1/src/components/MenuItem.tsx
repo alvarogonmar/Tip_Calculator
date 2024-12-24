5690283cf2a78793229d06423c3a772ae1a64b94
alvarogonmar <alvarogonmar0@gmail.com>
2024-12-23 21:20:44 -0600

    table

--- tip_calculator1/src/components/MenuItem.tsx
+++ tip_calculator1/src/components/MenuItem.tsx
@@ -5,9 +5,9 @@ type MenuItemProps = {
 };
 export default function MenuItem({ item }: MenuItemProps) {
   return (
-    <>
+    <button className=" border-2 border-teal-400 w-full hover:bg-teal-200 p-3 flex justify-between">
       <p>{item.name}</p>
       <p className="font-black">${item.price}</p>
-    </>
+    </button>
   );
 }
