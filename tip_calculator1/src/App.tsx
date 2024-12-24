99714aeaad842cb131976588c4b9eece10182f5c
alvarogonmar <alvarogonmar0@gmail.com>
2024-12-23 18:20:28 -0600

    header added

--- tip_calculator1/src/App.tsx
+++ tip_calculator1/src/App.tsx
@@ -1,7 +1,11 @@
 function App() {
   return (
     <>
-      <h1 className=" text-8xl">Hola mundo</h1>
+      <header className=" bg-teal-400 py-5">
+        <h1 className="text-center text-4xl font-black">
+          Tip and Total Pay Calculator
+        </h1>
+      </header>
     </>
   );
 }
