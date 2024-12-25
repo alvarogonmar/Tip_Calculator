25a5f05e55a35abdc17b5bcb76a460a016d05645
alvarogonmar <alvarogonmar0@gmail.com>
2024-12-24 18:17:13 -0600

    menu and prices edited with tailwind

--- tip_calculator1/src/App.tsx
+++ tip_calculator1/src/App.tsx
@@ -11,12 +11,13 @@ function App() {
       </header>
 
       <main className=" max-w-7xl py-20 mx-auto grid md:grid-cols-2">
-        <div>
-          <h2>Menu</h2>
-
-          {menuItems.map((item) => (
-            <MenuItem key={item.id} item={item} />
-          ))}
+        <div className="p-5">
+          <h2 className=" text-4xl font-black">Menu</h2>
+          <div className=" space-y-3 mt-10">
+            {menuItems.map((item) => (
+              <MenuItem key={item.id} item={item} />
+            ))}
+          </div>
         </div>
         <div>
           <h2>Consumption</h2>
