2189353a289b5d4996fb2c4acafdf21955470339
alvarogonmar <alvarogonmar0@gmail.com>
2024-12-26 19:32:32 -0600

    Showing the order, button X

--- tip_calculator1/src/components/OrderContents.tsx
+++ tip_calculator1/src/components/OrderContents.tsx
@@ -1,3 +1,4 @@
+import { formatCurrency } from "../helpers";
 import { OrderItem } from "../types";
 
 type OrderContentsProps = {
@@ -6,17 +7,29 @@ type OrderContentsProps = {
 export default function OrderContents({ order }: OrderContentsProps) {
   return (
     <div>
-      <h2 className=" font-black text-4xl">Consumption</h2>
+      <h2 className=" font-black text-4xl">Order</h2>
 
-      <div className="space-y-3 mt-5">
+      <div className="space-y-3 mt-10">
         {order.length === 0 ? (
-          <p className="text-center">La orden esta vacia</p>
+          <p className="text-center">Order Empty</p>
         ) : (
           order.map((item) => (
-            <div key={item.id}>
-              <p>
-                {item.name} - {item.price}
-              </p>
+            <div
+              key={item.id}
+              className="flex justify-between items-center border-t border-gray-200 py-5 last-of-type:border-b"
+            >
+              <div>
+                <p className="text-lg">
+                  {item.name} - {formatCurrency(item.price)}
+                </p>
+                <p className="font-black">
+                  Quantity: {item.quantity} -{" "}
+                  {formatCurrency(item.price * item.quantity)}
+                </p>
+              </div>
+              <button className="bg-red-600 h-8 w-8 rounded-full text-white font-black">
+                X
+              </button>
             </div>
           ))
         )}
