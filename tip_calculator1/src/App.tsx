5b0a418b59631743abf95e25cf18c11f1500d3b5
alvarogonmar <alvarogonmar0@gmail.com>
2024-12-26 18:31:59 -0600

    showing consumption

--- tip_calculator1/src/App.tsx
+++ tip_calculator1/src/App.tsx
@@ -1,4 +1,5 @@
 import MenuItem from "./components/MenuItem";
+import OrderContents from "./components/OrderContents";
 import { menuItems } from "./data/db";
 import useOrder from "./hooks/useOrder";
 
@@ -23,7 +24,7 @@ function App() {
           </div>
         </div>
         <div className=" border border-dashed border-slate-300 p-5 rounded-lg space-y-10">
-          <h2 className=" text-4xl font-black"></h2>
+          <OrderContents />
         </div>
       </main>
     </>
---
+++ tip_calculator1/src/components/OrderContents.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function OrderContents() {
+  return (
+    <div>
+      <h2 className=" font-black text-4xl">Consumption</h2>
+    </div>
+  );
+}
