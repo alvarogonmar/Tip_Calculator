b4419ed9741590d4f4fa900001dcb4fbe7e79d90
alvarogonmar <alvarogonmar0@gmail.com>
2024-12-26 20:02:22 -0600

    creating component for total pays

--- tip_calculator1/src/components/OrderTotals.tsx
+++ tip_calculator1/src/components/OrderTotals.tsx
@@ -1,3 +1,23 @@
 export default function OrderTotals() {
-  return <div>OrderTotals</div>;
+  return (
+    <>
+      <div className="space-y-3">
+        <h2 className="font-black text-2xl">Total and tip:</h2>
+        <p>
+          Subtotal to pay:
+          <span className="font-bold"> $0</span>
+        </p>
+        <p>
+          Tip:
+          <span className="font-bold"> $0</span>
+        </p>
+        <p>
+          Total to pay:
+          <span className="font-bold"> $0</span>
+        </p>
+      </div>
+
+      <button></button>
+    </>
+  );
 }
