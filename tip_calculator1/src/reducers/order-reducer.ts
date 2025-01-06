7d80bc693e1651dc6fc1d91e20410d9a665a3320
alvarogonmar <alvarogonmar0@gmail.com>
2025-01-06 17:43:33 -0600

    extend orderActions type to include remove-item, place-order, and add-tip actions

--- tip_calculator1/src/reducers/order-reducer.ts
+++ tip_calculator1/src/reducers/order-reducer.ts
@@ -1,4 +1,7 @@
 import { MenuItems } from "../types"
 
 export type orderActions = 
-    {type : "add-item", payload: {item: MenuItems}}
+    {type : "add-item", payload: {item: MenuItems}} |
+    {type : "remove-item", payload: {id: MenuItems["id"]}} |
+    {type : "place-order"} |
+    {type : "add-tip", payload: {value: number}}
