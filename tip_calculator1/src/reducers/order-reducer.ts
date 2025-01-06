2dcdfa6e6a0432348339f486886e854c6447f0f9
alvarogonmar <alvarogonmar0@gmail.com>
2025-01-06 17:45:19 -0600

    define OrderState type and initial state in order reducer

--- tip_calculator1/src/reducers/order-reducer.ts
+++ tip_calculator1/src/reducers/order-reducer.ts
@@ -1,7 +1,16 @@
-import { MenuItems } from "../types"
+import { MenuItems, OrderItem } from "../types"
 
 export type orderActions = 
     {type : "add-item", payload: {item: MenuItems}} |
     {type : "remove-item", payload: {id: MenuItems["id"]}} |
     {type : "place-order"} |
     {type : "add-tip", payload: {value: number}}
+
+export type OrderState = {
+    order: OrderItem[],
+    tip: number
+}
+export const initialState: OrderState = {
+    order: [],
+    tip: 0
+}
